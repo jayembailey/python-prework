7cc39a3de691f5545dda3d9c5aaa4bb268cdf2aa
Jacob Bailey <jayembailey@gmail.com>
2023-01-05 14:20:00 -0500

    "Prework Homework"

--- prework_homework.py
+++ prework_homework.py
@@ -3,36 +3,36 @@
 # USERNAME is the input of the function.
 # The first line of the code has been defined as below.
 
-# def hello_name(user_name):
-#     print('hello_' + user_name + '!')
+def hello_name(user_name):
+    print('hello_' + user_name + '!')
 
-# hello_name('USERNAME')
+hello_name('USERNAME')
 
 # Question 2
 # Write a python function, first_odds that prints
 # the odd numbers from 1-100 and returns nothing
 
-# def first_odds(nums):
-#     for num in nums:
-#         if num % 2 != 0:
-#             print(num)
+def first_odds(nums):
+    for num in nums:
+        if num % 2 != 0:
+            print(num)
 
-# first_odds(range(100))
+first_odds(range(100))
 
 # Question 3
 # Please write a Python function, max_num_in_list to return
 # the max number of a given list.
 # The first line of the code has been defined as below.
 
-# def max_num_in_list(a_list):
-#     a_list.sort()
-#     return a_list[-1]
+def max_num_in_list(a_list):
+    a_list.sort()
+    return a_list[-1]
 
-# max_num = max_num_in_list([1,5,2,9,3])
-# print(max_num)
+max_num = max_num_in_list([1,5,2,9,3])
+print(max_num)
 
-# max_num = max_num_in_list([38745,34587,9693874,52525252])
-# print(max_num)
+max_num = max_num_in_list([38745,34587,9693874,52525252])
+print(max_num)
 
 # Question 4
 # Write a function to return if the given year is a leap year.
@@ -40,13 +40,13 @@
 #     unless it is also divisible by 400.
 # The return should be boolean Type (true/false).
 
-# def is_leap_year(a_year):
-#     if a_year % 4 == 0 and a_year % 100 != 0 or a_year % 400 == 0:
-#         return True
-#     else:
-#         return False
-# year_question = is_leap_year(2400)
-# print(year_question)
+def is_leap_year(a_year):
+    if a_year % 4 == 0 and a_year % 100 != 0 or a_year % 400 == 0:
+        return True
+    else:
+        return False
+year_question = is_leap_year(2400)
+print(year_question)
 
 # Question 5
 # Write a function to check to see if all numbers in list are
